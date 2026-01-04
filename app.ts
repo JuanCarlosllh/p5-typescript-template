c5afaf3a0d84ac1626b49a9dc280baabbbbd153a
Juan Carlos Lloret <juancarloslloret93@gmail.com>
2026-01-04 11:53:32 +0100

    Minor style changes

--- app.ts
+++ app.ts
@@ -1,28 +1,25 @@
 import p5 from "p5";
 import init, { type p5SVG } from "p5.js-svg";
 
-const HTML_BG = "#EAE0CF";
-const CANVAS_BG = "#E0D3AF";
+const HTML_BG = "#1a1a1a";
+const CANVAS_BG = "#f6f6f6";
 
 const CANVAS_WIDTH = 595;
 const CANVAS_HEIGHT = 842;
 const CENTER_X = CANVAS_WIDTH / 2;
 const CENTER_Y = CANVAS_HEIGHT / 2;
+const SCALE = 0.8;
 
 // CSS variables
 document.documentElement.style.setProperty("--bg-color", HTML_BG);
 
 const USE_SVG = true;
-const SCALE = 0.8;
 
 if (USE_SVG) {
 	init(p5);
 }
 
 const s = (p: p5SVG) => {
-	const x = 100;
-	const y = 100;
-
 	p.setup = () => {
 		p.noLoop();
 		p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, p.SVG);
@@ -35,4 +32,8 @@ const s = (p: p5SVG) => {
 	};
 };
 
-new p5(s);
+const canvasElement = document.getElementById("canvas");
+if (!canvasElement) {
+	throw new Error("canvas container elemt not found");
+}
+new p5(s, canvasElement);
